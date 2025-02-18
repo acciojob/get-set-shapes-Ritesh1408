f0481036588d6b749f4665a22b2c6df1c4e12dca
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-02-18 22:44:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+    constructor(width, height) {
+        this._width = width;
+        this._height = height;
+    }
 
-class Square extends Animal {}
+    getArea() {
+        return this.width * this.height;
+    }
+
+    get width() {
+        return this._width;
+    }
+
+    get height() {
+        return this._height;
+    }
+
+    set width(width) {
+        this._width = width;
+    }
+
+    set height(height) {
+        this._height = height;
+    }
+}
+
+class Square extends Rectangle {
+    constructor(side) {
+        super(side, side); 
+    }
+
+    getPerimeter() {
+        return 4 * this.width; 
+    }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
